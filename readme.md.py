85eda3cd04b38cb0151e91c147f0941d901c95bc
snehagupta47103-stack <snehagupta47103@gmail.com>
2026-05-16 12:13:01 +0530

    Delete readme.md.py

--- readme.md.py
+++
@@ -1,6 +0,0 @@
-#Project 1: Fundamental Booster
-This is a Python script that collects user data, performs type casting, and shows memory adddresses.
-
-###Assumptios:
-- Current year is taken as 2026.
-- User provides valid numeric inputs for age and height.
