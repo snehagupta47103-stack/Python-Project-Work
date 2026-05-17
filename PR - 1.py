bd33f693b46ae4acb96c1ccf333050147c180e6f
snehagupta47103-stack <snehagupta47103@gmail.com>
2026-05-17 09:35:10 +0530

    Update PR - 1.py

--- PR - 1.py
+++ PR - 1.py
@@ -19,3 +19,21 @@ print(f"Your birth year is approximately : {birth_year}")
 print(f"(Based on your age of {age})")
 print(f"Note: Your height was converted from float {height} to integer {height_int} for rounding.")
 print("Thank you for using the personal data collector. Goodbye!")
+
+/*
+output :-
+Welcome to personal data collector
+Enter your name : Gupta Sneha 
+Enter your age :17
+Enter your height in meters: 1.68
+Enter your favourite number: 24
+Thank you! Here is the information we collected:
+ Gupta Sneha  <class 'str'> 2172749973808
+17 <class 'int'> 140706051966584
+1.68 <class 'float'> 2172791113008
+24 <class 'int'> 140706051966808
+Your birth year is approximately : 2006
+(Based on your age of 17)
+Note: Your height was converted from float 1.68 to integer 1 for rounding.
+Thank you for using the personal data collector. Goodbye!
+*/
